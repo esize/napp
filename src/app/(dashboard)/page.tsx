be9d209d693fb8aa630a72e98b2fb27bcc79f845
esize <evan@wool.homes>
2025-03-05 12:36:30 -0500

    Easy nav to users page

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,7 +1,13 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function HomePage() {
   return (
-    <div>
+    <div className="container m-4">
       <h1>Welcome!</h1>
+      <Button asChild>
+        <Link href="/admin/users">Users list</Link>
+      </Button>
     </div>
   );
 }
